e812db8f65205db398a58d30752d94f2fd1dc760
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-03 18:29:19 +0700

    add value property to Rating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import Rating from "./components/Rating/Rating";
+import Accordion from './components/Accordion/Accordion';
+import Rating from './components/Rating/Rating';
 
 // function declaration
 function App() {
     return (
         <div>
-            <PageTitle title={'This is App component'} />
-            <Rating />
-            <Accordion title={'Menu'} collapsed={false} />
-            <Rating />
+            <PageTitle title={ 'This is App component' } />
+            <Rating value={ 4 } />
+            <Accordion title={ 'Menu' } collapsed={ true } />
+            <Accordion title={ 'Users' } collapsed={ false } />
         </div>
     );
 }
 
-function PageTitle({title}: { title: string }) {
+function PageTitle({ title }: { title: string }) {
     return (
-        <h1>{title}</h1>
-    )
+        <h1>{ title }</h1>
+    );
 }
 
 export default App;
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,17 +1,21 @@
-export default function Rating() {
-    return (
-        <div>
-            <Star selected={true} />
-            <Star selected={true} />
-            <Star selected={true} />
-            <Star selected={false} />
-            <Star selected={false} />
-        </div>
-    )
+type RatingType = {
+    value: 0 | 1 | 2 | 3 | 4 | 5;
 }
 
-function Star({selected}: { selected: boolean }) {
+export default function Rating({ value }: RatingType) {
     return (
-        <span>{selected ? <b>star </b> : 'star '}</span>
-    )
+        <div>
+            <Star selected={ value > 0 } />
+            <Star selected={ value > 1 } />
+            <Star selected={ value > 2 } />
+            <Star selected={ value > 3 } />
+            <Star selected={ value > 4 } />
+        </div>
+    );
+}
+
+function Star({ selected }: { selected: boolean }) {
+    return (
+        <span>{ selected ? <b>star </b> : 'star ' }</span>
+    );
 }
