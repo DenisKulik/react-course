4762ae7e0f29e6c26259e7b429ba615de567b84b
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-10 19:21:01 +0700

    delete addRating function

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -3,20 +3,18 @@ import { useState } from 'react';
 export default function Rating() {
     const [ rating, setRating ] = useState(0);
 
-    const addRating = (value: number) => setRating(value);
-
     return (
         <div>
             <Star selected={ rating > 0 } />
-            <button onClick={ () => addRating(1) }>1</button>
+            <button onClick={ () => setRating(1) }>1</button>
             <Star selected={ rating > 1 } />
-            <button onClick={ () => addRating(2) }>2</button>
+            <button onClick={ () => setRating(2) }>2</button>
             <Star selected={ rating > 2 } />
-            <button onClick={ () => addRating(3) }>3</button>
+            <button onClick={ () => setRating(3) }>3</button>
             <Star selected={ rating > 3 } />
-            <button onClick={ () => addRating(4) }>4</button>
+            <button onClick={ () => setRating(4) }>4</button>
             <Star selected={ rating > 4 } />
-            <button onClick={ () => addRating(5) }>5</button>
+            <button onClick={ () => setRating(5) }>5</button>
         </div>
     );
 }
