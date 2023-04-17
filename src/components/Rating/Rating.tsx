518fadd94c81fc5638876e2ffdfb181ff40e8d3f
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-17 15:13:51 +0700

    refactor Rating component

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,26 +1,29 @@
 import { useState } from 'react';
 
 export default function Rating() {
-    const [ rating, setRating ] = useState(0);
+    const [ value, setValue ] = useState(0);
 
     return (
         <div>
-            <Star selected={ rating > 0 } />
-            <button onClick={ () => setRating(1) }>1</button>
-            <Star selected={ rating > 1 } />
-            <button onClick={ () => setRating(2) }>2</button>
-            <Star selected={ rating > 2 } />
-            <button onClick={ () => setRating(3) }>3</button>
-            <Star selected={ rating > 3 } />
-            <button onClick={ () => setRating(4) }>4</button>
-            <Star selected={ rating > 4 } />
-            <button onClick={ () => setRating(5) }>5</button>
+            <Star selected={ value > 0 } value={ 1 } setValue={ setValue } />
+            <Star selected={ value > 1 } value={ 2 } setValue={ setValue } />
+            <Star selected={ value > 2 } value={ 3 } setValue={ setValue } />
+            <Star selected={ value > 3 } value={ 4 } setValue={ setValue } />
+            <Star selected={ value > 4 } value={ 5 } setValue={ setValue } />
         </div>
     );
 }
 
-function Star({ selected }: { selected: boolean }) {
+type StarPropsType = {
+    selected: boolean
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
+}
+
+function Star({ selected, value, setValue }: StarPropsType) {
     return (
-        <span>{ selected ? <b>star </b> : 'star ' }</span>
+        <span onClick={ () => setValue(value) }>
+            { selected ? <b>star </b> : 'star ' }
+        </span>
     );
 }
