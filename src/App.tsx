aaf8a49f28f75a1b8f996755ba1b72fffca47110
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-08 22:31:08 +0700

    refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
 import './App.scss';
-import { useState } from 'react';
-import Accordion from './components/Accordion/Accordion';
-import Rating, { RatingValueType } from './components/Rating/Rating';
-import Switch from './components/Switch/Switch';
 
-function App() {
-    const [ ratingValue, setRatingValue ] = useState<RatingValueType>(0);
-    const [ accordionCollapsed, setAccordionCollapsed ] = useState(false);
-    const [ switchStatus, setSwitchStatus ] = useState(false);
-
-    const toggleCollapse = () => setAccordionCollapsed(!accordionCollapsed);
-
+const App = () => {
     return (
         <div className="App">
             <PageTitle title={'This is App component'} />
-            <Rating ratingValue={ratingValue} setRatingValue={setRatingValue} />
-            <Accordion
-                title={'Technologies'}
-                items={[]}
-                onClick={() => alert('some item was clicked')}
-                accordionCollapsed={accordionCollapsed}
-                toggleCollapse={toggleCollapse}
-            />
-            <Switch status={switchStatus} setStatus={setSwitchStatus} />
         </div>
     );
-}
+};
 
-function PageTitle({ title }: { title: string }) {
+const PageTitle = ({ title }: { title: string }) => {
     return (
         <h1>{title}</h1>
     );
-}
+};
 
 export default App;
