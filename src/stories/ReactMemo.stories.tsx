5ae4ee5587ee01340bb4d8c877075b562a07da6e
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-10 22:32:04 +0700

    add ReactMemo stories example

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+export default {
+    title: 'Components/ReactMemo'
+};
+
+const Counter = (props: { count: number }) => <div>{props.count}</div>;
+const UsersSecret = (props: { users: string[] }) => <div>{props.users.map(
+    (u, i) => <div key={i}>{u}</div>)}</div>;
+const Users = React.memo(UsersSecret);
+
+export const ExampleOne = () => {
+    const [ count, setCount ] = useState(0);
+    const [ users, setUsers ] = useState(
+        [ 'Anna', 'Denis', 'Alex', 'Sofia' ]);
+
+    return (
+        <>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setUsers([ ...users, 'Dymich' ])}>
+                add user
+            </button>
+            <Counter count={count} />
+            <Users users={users} />
+        </>
+    );
+};
